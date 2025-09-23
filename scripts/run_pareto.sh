141881e7ff2e30c888b48316caafaf23da720867
jsunn-y <jasonyang12345@gmail.com>
2025-09-22 20:35:50 -0700

    updated scripts to run experiments

--- scripts/run_pareto.sh
+++ scripts/run_pareto.sh
@@ -3,7 +3,7 @@ do
 
     python pareto.py pretrained_ckpt=continuous/$protein data=$protein model=continuous problem=protein_classifier_continuous algorithm=cls_guidance_continuous
 
-    python pareto.py pretrained_ckpt=continuous/$protein data=$protein model=continuous problem=protein_NOS_continuous algorithm=NOS_continuous
+    python pareto_NOS_hyperparameter.py pretrained_ckpt=continuous/$protein data=$protein model=continuous problem=protein_NOS_continuous algorithm=NOS_continuous
 
     python pareto.py pretrained_ckpt=continuous/$protein data=$protein model=continuous problem=protein_classifier_continuous algorithm=daps_continuous
 
@@ -15,11 +15,11 @@ do
 
     python pareto.py pretrained_ckpt=mdlm/$protein data=$protein model=mdlm problem=protein_classifier_discrete algorithm=daps_discrete
 
-    python pareto.py pretrained_ckpt=mdlm/$protein data=$protein model=mdlm problem=protein_NOS_discrete algorithm=NOS_discrete
+    python pareto_NOS_hyperparameter.py pretrained_ckpt=mdlm/$protein data=$protein model=mdlm problem=protein_NOS_discrete algorithm=NOS_discrete
 
 done
 
-for protein in TrpB CreiLOV
+for protein in TrpB CreiLOV GB1
 do  
 
     python pareto.py pretrained_ckpt=causalLM_finetune/$protein data=$protein model=causalLM problem=protein_DPO algorithm=DPO
